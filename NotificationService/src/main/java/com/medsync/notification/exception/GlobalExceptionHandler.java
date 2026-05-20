eea88d302623581922e628cd8d85a28cbd00a642
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-05-20 09:29:11 +0530

    add GlobalExceptionHandler for all exceptions

---
+++ NotificationService/src/main/java/com/medsync/notification/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.medsync.notification.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
