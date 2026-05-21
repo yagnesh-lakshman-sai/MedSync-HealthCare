8cd68473b6d74e36382f7c41ea3d0ac7b5261f1d
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-05-21 15:14:39 +0530

    add StaffAddressRepository dao

---
+++ StaffService/src/main/java/com/medsync/dao/StaffAddressRepository.java
@@ -0,0 +1,9 @@
+package com.medsync.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.medsync.model.StaffAddress;
+
+public interface StaffAddressRepository extends JpaRepository<StaffAddress, Long>{
+
+}
