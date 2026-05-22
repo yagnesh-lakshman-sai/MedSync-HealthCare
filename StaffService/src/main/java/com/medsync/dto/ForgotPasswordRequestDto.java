2e62eae6979c51cd05f6d38d3c7bfaea1adb100e
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-05-22 18:59:34 +0530

    add ForgotPasswordRequestDto class

---
+++ StaffService/src/main/java/com/medsync/dto/ForgotPasswordRequestDto.java
@@ -0,0 +1,12 @@
+package com.medsync.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ForgotPasswordRequestDto {
+    private String email;
+}
