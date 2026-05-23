3096439829b2c3b8c0a2656133947daae5b28340
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-05-23 20:17:22 +0530

    add VerifyOtpRequest

---
+++ StaffService/src/main/java/com/medsync/dto/VerifyOtpRequest.java
@@ -0,0 +1,14 @@
+package com.medsync.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VerifyOtpRequest {
+    private String email;
+    private String otp;
+}
