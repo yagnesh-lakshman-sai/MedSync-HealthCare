6edc0c64606038415ff2f2db08747a0bd4303a75
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-05-23 20:16:29 +0530

    add ResetPasswordRequest dto

---
+++ StaffService/src/main/java/com/medsync/dto/ResetPasswordRequest.java
@@ -0,0 +1,13 @@
+package com.medsync.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResetPasswordRequest {
+    private String email;
+    private String newPassword;
+}
