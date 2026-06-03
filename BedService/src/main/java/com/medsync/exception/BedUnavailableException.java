97b79e1632842ff5812979b8b457216138e9d1fe
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-06-03 20:32:24 +0530

    add BedUnavailableException

---
+++ BedService/src/main/java/com/medsync/exception/BedUnavailableException.java
@@ -0,0 +1,11 @@
+package com.medsync.exception;
+
+public class BedUnavailableException extends RuntimeException {
+	
+	    public BedUnavailableException(String message) {
+	        super(message);
+	    }
+	}
+
+
+
