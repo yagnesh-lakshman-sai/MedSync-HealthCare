460bca05dc1c0cd8bbd233a54c1c85d3f1cdfae5
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-06-08 19:56:12 +0530

    implement PatientServiceApplication project

---
+++ PatientService/src/main/java/com/medsync/PatientServiceApplication.java
@@ -0,0 +1,15 @@
+package com.medsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class PatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientServiceApplication.class, args);
+	}
+
+}
