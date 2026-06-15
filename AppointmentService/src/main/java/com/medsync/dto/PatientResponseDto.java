b5be21935ec56a60d59d12b81ab27b886a4c4d9c
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-06-15 19:19:38 +0530

    add PatientResponseDto

---
+++ AppointmentService/src/main/java/com/medsync/dto/PatientResponseDto.java
@@ -0,0 +1,33 @@
+package com.medsync.dto;
+
+
+import java.time.LocalDate;
+
+import com.medsync.utils.Gender;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PatientResponseDto {
+	
+	private String patientId;
+	
+	private String patientName;
+	
+	private Gender gender;
+	
+	private String patientEmail;
+	
+	private String patientPhoneNumber;
+	
+	private LocalDate dateOfBirth;
+
+	private PatientAddressResponseDto patientAddress;
+	
+}
