11d097831d55f15c98b3b873c000bfef013be56d
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-06-18 22:20:25 +0530

    add DoctorUnAvailableException

---
+++ AppointmentService/src/main/java/com/medsync/exception/DoctorUnAvailableException.java
@@ -0,0 +1,10 @@
+package com.medsync.exception;
+
+public class DoctorUnAvailableException extends RuntimeException {
+
+	public DoctorUnAvailableException(String message) {
+		super(message);
+	}
+
+	
+}
